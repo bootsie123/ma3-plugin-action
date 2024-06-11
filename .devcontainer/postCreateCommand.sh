80441a4b21144889b8af8f43b58fce079d085f49
bootsie123 <bootsie227@gmail.com>
2024-06-11 23:40:56 +0000

    update(file): - Fixed bash linter issues - Removed Shell SHFMT linter

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd ./
+cd ./ || exit
 npm install
 
-cd /tmp
+cd /tmp || exit
 wget https://github.com/nektos/act/releases/latest/download/act_Linux_x86_64.tar.gz && sudo tar -xvf act_Linux_x86_64.tar.gz -C /bin/ act
